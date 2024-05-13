6fe27cdc17c74045419fed3aab76afef6c7264c2
lucianefara <luciane.fara@hotmail.com>
2024-05-13 22:07:15 +0100

    Lógica de Programação - mod 1 e 2

---
+++ logica-js-projeto-inicial/app.js
@@ -0,0 +1,89 @@
+//Adivinhando o número secreto
+
+//alert('Boas vindas ao jogo do número secreto');
+
+//let numeroSecreto = 5;
+
+//let chute = prompt('Escolha um número entre 1 e 10');
+
+//if(numeroSecreto == chute){
+ //   console.log(`Você acertou o número secreto ${numeroSecreto}!!`);
+//} else{
+//    console.log('Tente novamente :(');
+//}
+
+
+
+//Exercício 1
+
+/*alert('Boas vindas ao nosso site');
+
+let nome = prompt('insira seu nome')
+
+let idade = prompt('insira sua idade')
+
+let numeroDeVendas = 50;
+
+let saldoDisponivel = 1000;*/
+
+//----------------------------------------------
+
+
+/*if(idade>=18){
+    alert('Você já pode tirar sua habilitação!!');
+    } 
+
+let mensagemDeErro = alert('Preencha todos os campos');*/
+
+//-----------------------------------------------
+
+
+//Exercício 1
+
+/*let diaDaSemana = prompt('Qual é o dia da semana?');
+
+if(diaDaSemana == 'sabado' | diaDaSemana == 'domingo'){
+  alert('Bom fim de semana!!')
+} else {
+    alert('Boa semana!!')
+}*/
+
+//Exercício 2
+
+/*let numeroDigitado = prompt('digite um numero');
+
+if(numeroDigitado<0){
+    alert('Você digitou um número negativo')
+} else {
+    alert('Você digitou um número positivo');
+}*/
+
+//Exercicio 3
+
+/*let pontuacao = prompt('Digite um valor');
+
+if(pontuacao>=100){
+  alert('Parabens, voce venceu!!');
+} else {
+   alert('Tente novamente para ganhar!!');
+}*/
+
+
+//Exercicio 4
+
+/*let saldoConta = 500; // Exemplo de saldo
+alert(`Seu saldo é de R$${saldoConta}.`);*/
+
+
+//Exercicio 5
+
+/*let nomeDoUsuario = prompt('Digite o seu nome');
+
+alert('Seja bem-vindo ' + nomeDoUsuario);*/
+
+
+
+
+
+
+
