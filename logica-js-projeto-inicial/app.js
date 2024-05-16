3b8024a16448b1c8e864bb416c066abbdb830958
lucianefara <luciane.fara@hotmail.com>
2024-05-16 16:34:49 +0100

    finalizacao programacao 1

--- logica-js-projeto-inicial/app.js
+++ logica-js-projeto-inicial/app.js
@@ -1,16 +1,52 @@
 //Adivinhando o número secreto
 
-alert('Boas vindas ao jogo do número secreto');
+//alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 5;
+//math.random é um número PSEUDO aleatório entre 0 e 1 mas não incluso o 1.
+//parseInt é aceito apenas os números inteiros
 
-let chute = prompt('Escolha um número entre 1 e 10');
+/*let numeroSecreto = parseInt(Math.random() *10+1 );
 
-if(numeroSecreto == chute){
-  console.log(`Você acertou o número secreto ${numeroSecreto}!!`);
-} else{
-    console.log('Tente novamente :(');
-}
+let chute;
+let contador = 1;
+
+while(numeroSecreto != chute){
+
+  chute = prompt('Digite o número secreto de 1 a 10');
+
+    if(numeroSecreto == chute){
+          
+          break;
+
+        } else{
+
+          if(numeroSecreto > chute){
+                alert('O número secreto é maior que ' + chute);
+              }  else {
+                alert('O número secreto é menor que ' + chute);
+              }                 
+              
+              contador++ ;   
+
+        }       
+} */
+
+
+//outra forma para diminuir o número de linhas de código
+
+//OPERADOR TERNÁRIO
+
+//let palavraTentativa = contador > 1 ? 'Tentativas' : 'Tentativa';
+//alert(`Você acertou o número secreto ${numeroSecreto} com ${contador} ${palavraTentativa}!!`);
+
+
+/*if(contador>1){
+  alert(`Você acertou o número secreto ${numeroSecreto} com ${contador} vezes!!`);
+} else {
+  alert(`Você acertou o número secreto ${numeroSecreto} com ${contador} vez!`)
+}*/
+
+
 
 
 
@@ -80,6 +116,93 @@ alert(`Seu saldo é de R$${saldoConta}.`);*/
 /*let nomeDoUsuario = prompt('Digite o seu nome');
 
 alert('Seja bem-vindo ' + nomeDoUsuario);*/
+
+
+//Desafio: hora da prática
+
+//Exercício 1
+
+//console.log('Boas-vindas');
+
+/*Exercício 2 até 13
+
+let nome = 'Luciane';
+
+console.log(`olá, ${nome}`);
+alert(`Olá, ${nome}`);
+
+let linguagem = prompt('Qual a linguagem de programação que você mais gosta?');
+
+console.log(`A linguagem de preferencia é o ${linguagem}`);
+
+
+let valor1=1;
+let valor2=2;
+
+let result= valor1+valor2;
+let resulSub = valor1-valor2;
+
+console.log(`A soma de  ${valor1} +  ${valor2} é igual a ${result}`);
+
+console.log(`A subtração de  ${valor1} -  ${valor2} é igual a ${resulSub}`);
+
+
+let idade = prompt('Digite a sua idade');
+
+//let palavraTentativa = contador > 1 ? 'Tentativas' : 'Tentativa';
+
+let verifica = idade > 18 ? 'Você é maior de idade' : 'Você é menor de idade';
+
+console.log(verifica);
+
+
+let numero = prompt('Insira um valor');
+
+let verificando = numero > 0 ? 'O número é positivo' : numero == 0 ? 'o número é zero' : 'número é negativo';
+
+console.log(verificando);
+
+let numeros = 0;
+
+while(numeros<10){
+
+  console.log(numeros);
+
+  numeros++;
+
+}
+
+let nota = 0;
+
+if(nota>7){
+  console.log('Você foi aprovado');
+} else {
+  console.log('Você não foi aprovado');
+}
+
+let numeroAleat = Math.random();
+
+console.log(numeroAleat);
+*/
+
+let numeroInteiroAleatorio = parseInt(Math.random()*10)+1;
+
+console.log(numeroInteiroAleatorio);
+
+
+let numeroInteiroAleatorio2 = parseInt(Math.random()*1000)+1;
+
+console.log(numeroInteiroAleatorio2);
+
+
+
+
+
+
+
+
+
+
 
 
 
